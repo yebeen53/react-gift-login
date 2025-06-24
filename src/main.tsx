5d94b730c857182a4aa7faa18a1360537bcc172f
Yebeen <yebeenlee0503@naver.com>
2025-06-24 10:57:50 +0900

    main alias path

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import GlobalStyles from './emotionstyle';
+import '@fontsource/pretendard/variable.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles/>
     <App />
   </StrictMode>,
 )
