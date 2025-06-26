4179adb6a5dbbaa4c14c6f00f6bd6cabc2efcd9c
Yebeen <yebeenlee0503@naver.com>
2025-06-26 17:05:19 +0900

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
-import './index.css'
 import App from './App.tsx'
 import GlobalStyles from './emotionstyle.tsx';
 import '@fontsource/pretendard/400.css';
