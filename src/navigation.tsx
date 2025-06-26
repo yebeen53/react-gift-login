d09668600fbb21ace478efe0b3da8f5c8de27f3b
Yebeen <yebeenlee0503@naver.com>
2025-06-26 16:58:08 +0900

    add navigation bar

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,25 +1,60 @@
-/**@jsxImportSource @emotion/react */
-import { useTheme } from '@emotion/react';
+/** @jsxImportSource @emotion/react */
+import { css, useTheme } from '@emotion/react';
 import type { Theme } from './theme';
 
+const Navibar = () => {
+  const theme = useTheme() as Theme;
 
-const Navibar=()=>{
-    const theme = useTheme() as Theme;
-    const navstyle=`
-            max-width:720px;
-            height: auto;
-            background:${theme.colors.semantic.kakaoYellow};
-            display:flex;
-            align-items:center;
-            color:${theme.colors.gray1000};
-            margin: 0 auto;`
+  const container = css`
+    max-width: 720px;
+    height: 50px;
+    background: ${theme.colors.semantic.backgroundDefault};
+    display: flex;
+    align-items: center;
+    justify-content: space-between; /* 세 요소 좌우 배치 */
+    margin: 0 auto;
+    padding: 0 16px;
+  `;
 
-    return (<>
-        <nav>
-            css={navstyle}
-            </nav>
-                선물하기
-    
-    </>)
+  const navstyle = css`
+    font-size: ${theme.typography.title1Bold.fontSize};
+    font-weight: ${theme.typography.title1Bold.fontWeight};
+    line-height: ${theme.typography.title1Bold.lineHeight};
+    color: ${theme.colors.semantic.textDefault};
+  `;
 
-}; export default Navibar;
+  const per = css`
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6aLBdZ-dWr4tVuP38je4WW2WVGphxmiINHA&s');
+    background-size: contain;
+    background-position: center;
+    `
+
+
+  const pre = css`
+    cursor: pointer;
+  `;
+
+  const preStyle = css`
+    width: 40px;
+    height: 40px;
+    color: black;
+    font-size: 25px;
+  `;
+
+  return (
+    <div css={container}>
+      <div css={pre}>
+        <div css={preStyle}>&lt;</div>
+      </div>
+
+      <nav css={navstyle}>선물하기</nav>
+
+      <div css={per}></div>
+    </div>
+  );
+};
+
+export default Navibar;
