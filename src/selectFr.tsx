5278cbe27f8957892f908d4d11a80b9499b2bf5c
Yebeen <yebeenlee0503@naver.com>
2025-06-27 21:51:27 +0900

    refactor: css out of component

--- src/selectFr.tsx
+++ src/selectFr.tsx
@@ -1,16 +1,15 @@
 import { css} from '@emotion/react';
 import hook from './hook'
-const SelectFr = () => {
-  const theme = hook();
+import type {Theme} from '@/theme'
 
-  const containerStyle = css`
+const containerStyle = (theme:Theme)=>css`
     padding: ${theme.spacing.spacing4};
     display: flex;
     flex-direction: column;
     gap: ${theme.spacing.spacing4};
     height: 80px;
   `;
-  const friendBoxStyle = css`
+  const friendBoxStyle =(theme:Theme)=> css`
     display: flex;
     align-items: center;
     gap: ${theme.spacing.spacing2};
@@ -18,7 +17,7 @@ const SelectFr = () => {
     border-radius: 8px;
   `;
 
-  const plusCircleStyle = css`
+  const plusCircleStyle = (theme:Theme)=>css`
     width: 40px;
     height: 40px;
     background-color: ${theme.colors.semantic.kakaoYellow};
@@ -31,21 +30,22 @@ const SelectFr = () => {
     justify-content: center;
   `;
 
-  const bannerStyle = css`
+  const bannerStyle =(theme:Theme)=> css`
     background-color: ${theme.colors.semantic.backgroundDefault};
     padding: ${theme.spacing.spacing2};
     font-size: ${theme.typography.label2Regular.fontSize};
     border-radius: 8px;
   `;
-
+  
+const SelectFr = () => {
+  const theme = hook();
   return (
-    <div css={containerStyle}>
-      <div css={friendBoxStyle}>
-        <div css={plusCircleStyle}>+</div>
+    <div css={containerStyle(theme)}>
+      <div css={friendBoxStyle(theme)}>
+        <div css={plusCircleStyle(theme)}>+</div>
         <span>선물할 친구를 선택해 주세요.</span>
       </div>
-
-      <div css={bannerStyle}>
+      <div css={bannerStyle(theme)}>
       </div>
     </div>
   );
