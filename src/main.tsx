a2ab30fa80eed496a924f09bce99aab1f284f025
Yebeen <yebeenlee0503@naver.com>
2025-06-27 16:09:17 +0900

    refactor(path)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@emotion/react';
-import theme from './theme';
-import App from './App.tsx'
-import GlobalStyles from './emotionstyle.tsx';
+import theme from '@/theme';
+import App from '@/App.tsx'
+import GlobalStyles from '@/emotionstyle.tsx';
 import '@fontsource/pretendard/400.css';
 import category from '../mockdata';
 createRoot(document.getElementById('root')!).render(
